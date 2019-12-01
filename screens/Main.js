4309ad71438ce499d2456ff2daaa30fe59d1b906
Eduardo Finotti DEV <eduardofinottidev@eduardo-finotti.local>
2019-11-30 23:39:18 -0300

    serach local

--- screens/Main.js
+++ screens/Main.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
-import {Dimensions, StyleSheet, Image, View, Alert, ScrollView, TouchableOpacity, Text, TextInput } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import RBSheet from "react-native-raw-bottom-sheet";
+import { TouchableOpacity, StyleSheet, Image, View, Alert, Button, Text, TextInput } from 'react-native';
 import AddPlace from './addPlace'
 import MapView, {Marker} from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
+import Modal from 'react-native-modal';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import LinearGradient from 'react-native-linear-gradient'
+import osso from './assets/osso.png'
+import Search from './Search';
 
 export default class Main extends Component {
-  
+
+
+  open = (title, address) => {
+    this.setState({visible: true, title: title, address: address} )
+  };
+
+  close = () => this.setState({visible: false} );
+  isVisible = () => this.state.visible;
+
   state = {
+    title: null,
+    address: null,
+    visible: false,
     region: null,
     markers:[
       {
@@ -23,33 +37,28 @@ export default class Main extends Component {
         title: "Parque Amarubá",
         address: "Avenida das Naçoes, 4523, KM-2"
       },
+      {
+        lat: -27.5986393,
+        lon: -48.5187229,
+        title: "Parque Santa Monica",
+        address: "Avenida dos caras, 4569 - SN"
+      },
     ]
   }
 
-  show = data => {
+  changeLocation = data => {
+    try {
+      const region = {
+        latitude: data.lat,
+        longitude: data.lon,
+        latitudeDelta: 0.06,
+        longitudeDelta: 0.06,
+      }
+      this.setState({ region });
 
-    // this.state.markers.push(data);
-    
-    // this.setState({markers:this.state.markers});
-    
-    // console.log(this.state)
-
-            // ADD TASKS QUANDO SALVANSO NO DISPOSITIVO
-        var local = [...this.state.markers]
-        local.push({
-            lat: data.lat ,
-            lon: data.lon,
-            title: data.title,
-            address: data.address
-        })
-
-        this.setState({ markers: local })
-
-    // this.componentDidMount()
-  }
-
-  componentDidMount() {
-    this.getCurrentPosition();
+    } catch(e) {
+      alert(e.message || "");
+    }
   }
 
   getCurrentPosition() { 
@@ -73,124 +82,193 @@ export default class Main extends Component {
     }
   };
 
+  show = data => {
+    var local = [...this.state.markers]
+    local.push({
+        lat: data.lat ,
+        lon: data.lon,
+        title: data.title,
+        address: data.address
+    })
+
+    this.setState({ markers: local })
+  }
+
+  componentDidMount() {
+    this.getCurrentPosition();
+  }
+
   render() {
 
     return (
       <View style={styles.container}> 
-
+          
           <MapView 
-            style={styles.map2}
+            style={styles.mapView}
             region={ this.state.region }
             showsUserLocation={true}
             loadingEnabled={true} 
-            howsMyLocationButton={true}
-          >
+            howsMyLocationButton={true}>
 
             {this.state.markers.map((marker, index) => (
               <MapView.Marker style={{alignItems: 'center', paddingBottom: 80}}
                 key={index} 
                 coordinate={{latitude: marker.lat, longitude: marker.lon}}
-                onPress={() => this.Scrollable.open()}
+                onPress={() => this.open(marker.title, marker.address)}
                 width={10} height={10}
                 description={marker.description} >
                 <Image source={require('./assets/local.png')} style={{height: 50, width: 50}} />
-                  
-                  <RBSheet 
-                    ref={ref => { this.Scrollable = ref; }}
-                    closeOnDragDown
-                    height = { 500 }
-                    customStyles={{
-                      container: {
-                          borderTopLeftRadius: 10,
-                          borderTopRightRadius: 10 }}}>
-
-                    <ScrollView style={styles.dialog} >
-                        <View style={styles.gridContainer} >
-                            <View style={styles.containerView}>
-                                <Text style={styles.titleLocal}>{marker.title}</Text>
-                              
-                                <View style={styles.input}>
-                                    <Icon name={'map-pin'} size={20} style={styles.icon} />
-                                    <TextInput style={styles.addresslabel}>{marker.address}</TextInput>
-                                </View>
-                            </View>
-                        </View>
-
-                    </ScrollView>
-                  </RBSheet>
-
               </MapView.Marker>
             ))}
 
           </MapView>
 
-          <View style={styles.place}> 
-            <AddPlace onSave={this.show} tamanho={800} /> 
+          <View style={styles.container2}>
+            <Search onSearch={this.changeLocation}/>
           </View>
           
+          <View style={styles.container3}>
+            <AddPlace onSave={this.show} tamanho={800} /> 
+          </View>
+
+          <View > 
+            <Modal
+              testID={'modal'}
+              isVisible={this.state.visible}
+              onSwipeComplete={this.close}
+              swipeDirection={['down']}
+              style={styles.modal}>
+
+              <View style={styles.scrollableModal}>
+                <Text style={{fontSize: 30, color: '#FFF'}} > ___ </Text>
+                  <View style={styles.scrollableModalContent1}>
+
+                      <View style={{alignItems:'center'}}>
+                        <Text style={styles.scrollableModalText1}>{this.state.title} </Text>
+                      </View>
+                      
+                      <View style={{paddingTop:20}}>
+                        <View style={styles.input}>
+                            <Icon name={'map-pin'} size={20} />
+                            <TextInput style={styles.addresslabel}>{this.state.address}</TextInput>
+                        </View>
+                      </View>
+                      <View style={styles.itens}>
+                        <TouchableOpacity style={{}}>
+                            <LinearGradient style={styles.contentCarac} colors={['#F7651C', '#F71C73']}>
+                                <Image style={styles.osso} source={osso}></Image>
+                            </LinearGradient>
+                        </TouchableOpacity>
+                      </View>  
+                  </View>
+              </View>
+
+            </Modal>
+        
+          </View>
+
         </View>
       );
   }
 }
 
-     
-        {/* <ImageBackground style={styles.map} source={map}> */}
-          {/* <AddPlace tamanho={500} register={false}/>
-          <AddPlace tamanho={800} register={true}/> */}
-        {/* </ImageBackground> */}
-
 const styles = StyleSheet.create({
-  map: {
-    flex: 1, 
-    justifyContent: 'space-around',
-    alignItems: 'flex-end',
-    flexDirection: "row",
-    paddingBottom: 30,
-    paddingLeft: 140,
-    paddingRight: 20,
-  },
 
   container: {
     flex: 1,
-    justifyContent: "flex-end",
-    width: '100%',
   },
 
-  map2: {
+  mapView: {
     flex: 1,
+  },
+
+  container2: {
+    flexDirection: "row",
+    alignContent: "center",
+    justifyContent: "center",
+    position: 'absolute',
     
   },
 
-  place:{
-    position: 'absolute',
-    padding: 50,
-    paddingLeft: 280
+  container3: {
+    position: 'absolute',//use absolute position to show button on top of the map
+    top: '85%', //for center align
+    right: '10%',
+    alignSelf: 'flex-end' //for align to right
   },
 
-  gridContainer: {
-  },
-
-  gridIcon: {
-    fontSize: 30,
-    color: "white"
-},
-
-containerView: {
-    alignItems: "center"
-},
-
-dialog: {
-  backgroundColor: '#262B56',
-},
-
-containerClose: {
-  flexDirection: "row",
-  justifyContent: 'flex-end',
-  paddingRight: 20,
-  paddingTop: 10
-},
 
 titleLocal: {
+  fontSize:25,
+  color: 'white',
+  padding: 20,
+  justifyContent: 'center',
+  alignContent: 'center',
+  alignItems: 'center',
+},
+
+addresslabel: {
+  textAlign: "left",
+  width: '80%',
+  fontSize: 17,
+  color: 'black',
+  paddingLeft: 20
+},
+
+input: {
+  width: '100%',
+  height: 40,
+  backgroundColor: '#EEE',
+  borderRadius: 20,
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: "flex-start",
+  paddingLeft: 20,
+},
+
+contentCarac: {
+  width: 150,
+  height: 90,
+  borderRadius: 20,
+  borderColor: 'transparent',
+  backgroundColor: "#F46721",
+  alignItems: 'center',
+  alignContent: 'center',
+  justifyContent: 'center'
+},
+
+osso: {
+  width: 40,
+  height: 40,
+},
+
+itens: {
+  flex: 1,
+  // flexDirection: "row",
+  width: '100%',
+  paddingTop: 20
+},
+
+modal: {
+  justifyContent: 'flex-end',
+  margin: -5,
+},
+scrollableModal: {
+  //tamanho do dialog
+  height: '60%',
+  width: '100%',
+  backgroundColor: '#262B56',
+  borderRadius: 40,
+  alignItems: 'center',
+},
+scrollableModalContent1: {
+  //tamanho do scroll
+  // height: '100%',
+  backgroundColor: '#262B56',
+  justifyContent: 'center',
+},
+
+title: {
   fontSize:25,
   color: 'white',
   padding: 20,
@@ -199,27 +277,24 @@ titleLocal: {
   alignItems: 'center'
 },
 
-addresslabel: {
-  fontSize: 15,
-  color: 'black',
-  paddingLeft: 30,
+scrollableModalText1: {
+  fontSize:25,
+  color: 'white',
+  paddingTop: 10,
+  justifyContent: 'center',
+  alignContent: 'center',
+  alignItems: 'center'
 },
 
-input: {
-  width: '80%',
-  height: 40,
-  paddingLeft: 10,
-  backgroundColor: '#EEE',
-  borderRadius: 20,
-  flexDirection: 'row',
-  alignItems: 'center',
-  justifyContent: "flex-start",
-  paddingLeft: 22
-},
-
-carac2: {
-  // flexDirection: "row",
-  paddingTop: 20,
+buttonView: {
+  borderWidth:1,
+  borderColor:'#FFF',
+  alignItems:'center',
+  justifyContent:'center',
+  width:80,
+  height:80,
+  backgroundColor:'red',
+  borderRadius:50,
 },
 
 });
